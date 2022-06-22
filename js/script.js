6cada87084ea978741127916a507c1f81b1ea31f
Selinda-C <selindachapman@icloud.com>
2022-06-21 20:29:50 -0700

    add to upper case function to items

--- js/script.js
+++ js/script.js
@@ -3,18 +3,18 @@ let form = document.getElementById("groceries")
 
 form.addEventListener("submit", function(event) { 
   event.preventDefault();
-  let item1 = document.querySelector("input#item1").value;
-  let item2 = document.querySelector('input#item2').value;
-  let item3 = document.querySelector("input#item3").value;
+  let item1 = document.querySelector("input#item1").value.toUpperCase();
+  let item2 = document.querySelector('input#item2').value.toUpperCase();
+  let item3 = document.querySelector("input#item3").value.toUpperCase();
   const arrayOfItems = [item1, item2, item3];
 
   let sortedArray = arrayOfItems.sort();
   
   sortedArray.forEach(function(item){
-    const itemList = document.getElementById("itemList")
+    const itemList = document.querySelector("ul")
     const lineItem = document.createElement("li");
-    const addItem = lineItem.append(item)
-    itemList.append(addItem)
+    lineItem.append(item)
+    itemList.append(lineItem)
   });
-})
-}
+});
+};
