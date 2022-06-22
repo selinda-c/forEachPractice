df93d040abcac70a214c1147b3d7d1ad343602c5
Selinda-C <selindachapman@icloud.com>
2022-06-21 20:13:45 -0700

    WIP displaying undefined on submit

--- js/script.js
+++ js/script.js
@@ -1,15 +1,20 @@
 window.onload = function() {
-
-let item1 = document.querySelector("item1");
-let item2 = document.querySelector('item2');
-let item3 = document.querySelector("item3");
-const arrayOfItems = [item1, item2, item3];
 let form = document.getElementById("groceries")
 
-let sortedArray = arrayOfItems.sort();
-
 form.addEventListener("submit", function(event) { 
-  
   event.preventDefault();
+  let item1 = document.querySelector("input#item1").value;
+  let item2 = document.querySelector('input#item2').value;
+  let item3 = document.querySelector("input#item3").value;
+  const arrayOfItems = [item1, item2, item3];
+
+  let sortedArray = arrayOfItems.sort();
+  
+  sortedArray.forEach(function(item){
+    const itemList = document.getElementById("itemList")
+    const lineItem = document.createElement("li");
+    const addItem = lineItem.append(item)
+    itemList.append(addItem)
+  });
 })
 }
