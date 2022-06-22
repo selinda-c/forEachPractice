8fe7e01828c616ebe86337d8b236e3396b1dbe5a
Selinda-C <selindachapman@icloud.com>
2022-06-21 20:53:19 -0700

    update function to capitalize 1st letter instead of whole word

--- js/script.js
+++ js/script.js
@@ -3,10 +3,13 @@ let form = document.getElementById("groceries")
 
 form.addEventListener("submit", function(event) { 
   event.preventDefault();
-  let item1 = document.querySelector("input#item1").value.toUpperCase();
-  let item2 = document.querySelector('input#item2').value.toUpperCase();
-  let item3 = document.querySelector("input#item3").value.toUpperCase();
-  const arrayOfItems = [item1, item2, item3];
+  let item1 = document.querySelector("input#item1").value;
+  let item2 = document.querySelector('input#item2').value;
+  let item3 = document.querySelector("input#item3").value;
+  i1 = item1[0].toUpperCase() + item1.slice(1);
+  i2 = item2[0].toUpperCase() + item2.slice(1);
+  i3 = item3[0].toUpperCase() + item3.slice(1);
+  const arrayOfItems = [i1, i2, i3];
 
   let sortedArray = arrayOfItems.sort();
   
